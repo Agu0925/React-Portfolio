d2170b7f25def4761a5e4f1f395f3524fca0584d
Agustin Ahlers <agustin.ahlers@hotmail.com>
2023-10-19 19:58:55 -0300

    prueba7

--- src/Index.jsx
+++ src/Index.jsx
@@ -7,7 +7,7 @@ import css from './assets/Iconos/css.ico'
 import bs from './assets/Iconos/bs.ico'
 import js from './assets/Iconos/js.ico'
 import node from './assets/Iconos/node.ico'
-import reactimg from './assets/Iconos/react.ico'
+//import reactimg from './assets/Iconos/react.ico'
 //Titulo ---------------------------
 const TextoMaquinaEscribir = ({ text }) => {
   const [displayText, setDisplayText] = useState("");
