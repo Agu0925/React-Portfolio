86d65200772a709710f351eaaf0c9d61ef1aad72
Agustin Ahlers <agustin.ahlers@hotmail.com>
2023-10-19 18:51:12 -0300

    cambio en vite config y gith deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/React-Portfolio/",
+  base: "https://agu0925.github.io/React-Portfolio/",
   plugins: [react()],
 })
